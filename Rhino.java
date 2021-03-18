d1f85ca24c488433fd41f338821a2f1c418ffb1b
shihminlee <shih-min.lee@prodigygame.com>
2021-03-18 14:40:51 -0400

    x

--- Rhino.java
+++ Rhino.java
@@ -10,7 +10,7 @@ import java.io.File;  // Import the File class
 import java.io.FileNotFoundException;  // Import this class to handle errors
 import java.util.Scanner; // Import the Scanner class to read text files
 
-public class Rh {
+public class Rhino {
     static String getCode(String filename) {
         try {
             File obj = new File(filename);
